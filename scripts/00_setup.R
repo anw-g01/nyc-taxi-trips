d95b4b1ed48dd59ea1109cd96a9f2eac9033150e
anw-g01 <anw_g01@outlook.com>
2026-03-10 18:31:05 +0000

    add panel tabs in quarto report (+ define global variables)

--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -20,3 +20,15 @@ CLEAN   <- here("data", "clean")
 MARTS   <- here("data", "marts")
 REPORTS <- here("reports")
 FIGURES <- here("figures")
+
+# define global variables for visualisations and outputs
+
+BASE_FAMILY     <- "Palatino Linotype"      # base font for plots 
+BASE_SIZE       <- 11                       # base font size
+
+COLOUR_PALETTE  <- paletteer_d("MoMAColors::Abbott")
+COLOUR_GREEN    <- COLOUR_PALETTE[[5]]    # Green Taxi
+COLOUR_UBER     <- COLOUR_PALETTE[[1]]    # Uber 
+COLOUR_OTHER    <- COLOUR_PALETTE[[7]]    # Other
+
+FIG_CAPTION     <- "Source: NYC Taxi & Limousine Commission (TLC) Trip Record Data (2021)" 
